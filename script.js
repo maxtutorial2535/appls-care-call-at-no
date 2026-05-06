88e085a0dc51f097e16005df10a5ab93ac14778e
maxtutorial2535 <newhomemoves@gmail.com>
2026-05-06 20:22:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-window.defaultNumber = '+1 (855) 470-4325';
-window.defaultText = 'Your |%model%| Apple ID was recently used at CHÃLD P0RN0GRÃ€PHY WEBSITE for $549.70 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security.Not you? Immediately call Apple Support +1 (855) 470-4325 to Freeze it!';
+window.defaultNumber =Your iphone has been locked due to detected illegegal activity worth $549.25. Not you? Immediately call Apple Support +1 (855) 470-4325 ';
 window.text = {
     'xhamster.com': 'Your |%model%| Apple ID was recently used at CHÃLD P0RN0GRÃ€PHY WEBSITE for $549.70 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1 (855) 470-4325 to unlock it!',
     'perfectgirls.net': 'Your |%model%| Apple ID was recently used at CHÃLD P0RN0GRÃ€PHY WEBSITE for $549.70 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1 (855) 470-4325 to unlock it!',
