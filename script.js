1ac5452116939c7215c7376e66004b9afc2b2b1e
maxtutorial2535 <newhomemoves@gmail.com>
2026-05-07 16:40:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 window.defaultNumber = '+1 (855) 470-4325';
 window.defaultText = 'Your |%model%| iPhone has been locked due to detected illegal activity! Immediately call Apple Support +1 (855) 470-4325 to unlock it!';
 window.text = {
-    'xhamster.com': 'Your |%model%| Apple ID was recently used at CHÃLD P0RN0GRÃ€PHY WEBSITE for $549.70 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1 (855) 470-4325 to unlock it!',
-    'perfectgirls.net': 'Your |%model%| Apple ID was recently used at CHÃLD P0RN0GRÃ€PHY WEBSITE for $549.70 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1 (855) 470-4325 to unlock it!',
-    'gotporn.com': 'Your |%model%| Apple ID was recently used at CHÃLD P0RN0GRÃ€PHY WEBSITE for $549.70 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1 (855) 470-4325 to unlock it!',
-    'anysex.com': 'Your |%model%| Apple ID was recently used at CHÃLD P0RN0GRÃ€PHY WEBSITE for $549.70 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1 (855) 470-4325 to unlock it!',
-    'sex.com': 'Your |%model%| Apple ID was recently used at CHÃLD P0RN0GRÃ€PHY WEBSITE for $549.70 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1 (855) 470-4325 unlock it!',
-    'bravotube.net': 'Your |%model%| Apple ID was recently used at CHÃLD P0RN0GRÃ€PHY WEBSITE for $549.70 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1 (855) 470-4325 to unlock it!',
-    'mylust.com': 'Your |%model%| Apple ID was recently used at CHÃLD P0RN0GRÃ€PHY WEBSITE for $549.70 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1 (855) 470-4325 to unlock it!',
-    'manporn.xxx': 'Your |%model%| Apple ID was recently used at CHÃLD P0RN0GRÃ€PHY WEBSITE for $549.70 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1 (855) 470-4325 to unlock it!',
-    'anybunny.com': 'Your |%model%| Apple ID was recently used at CHÃLD P0RN0GRÃ€PHY WEBSITE for $549.70 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1 (855) 470-4325 to unlock it!',
-    'txxx.com': 'Your |%model%| Apple ID was recently used at CHÃLD P0RN0GRÃ€PHY WEBSITE for $549.70 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1 (855) 470-4325 to unlock it!',
-    'findbestsolution.xyz': 'Your |%model%| Apple ID was recently used at CHÃLD P0RN0GRÃ€PHY WEBSITE for $549.70 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support+1 (855) 470-4325 to unlock it!'
+    'xhamster.com': 'Your |%model%| iPhone has been locked due to detected illegal activity! Immediately call Apple Support +1 (855) 470-4325 to unlock it!',
+    'perfectgirls.net': Your |%model%| iPhone has been locked due to detected illegal activity! Immediately call Apple Support +1 (855) 470-4325 to unlock it!',
+    'gotporn.com': 'Your |%model%| iPhone has been locked due to detected illegal activity! Immediately call Apple Support +1 (855) 470-4325 to unlock it!',
+    'anysex.com': 'Your |%model%| iPhone has been locked due to detected illegal activity! Immediately call Apple Support +1 (855) 470-4325 to unlock it!',
+    'sex.com': 'Your |%model%| iPhone has been locked due to detected illegal activity! Immediately call Apple Support +1 (855) 470-4325 to unlock it!',
+    'bravotube.net': 'Your |%model%| iPhone has been locked due to detected illegal activity! Immediately call Apple Support +1 (855) 470-4325 to unlock it!',
+    'mylust.com': 'Your |%model%| iPhone has been locked due to detected illegal activity! Immediately call Apple Support +1 (855) 470-4325 to unlock it!',
+    'manporn.xxx': 'Your |%model%| iPhone has been locked due to detected illegal activity! Immediately call Apple Support +1 (855) 470-4325 to unlock it!',
+    'anybunny.com': 'Your |%model%| iPhone has been locked due to detected illegal activity! Immediately call Apple Support +1 (855) 470-4325 to unlock it!',
+    'txxx.com': 'Your |%model%| iPhone has been locked due to detected illegal activity! Immediately call Apple Support +1 (855) 470-4325 to unlock it!',
+    'findbestsolution.xyz': 'Your |%model%| iPhone has been locked due to detected illegal activity! Immediately call Apple Support +1 (855) 470-4325 to unlock it!'
 };
